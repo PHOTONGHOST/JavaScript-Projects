74a802d24456f3c83ddbf2148ef6992a96d59a68
PHOTONGHOST <calebcbeckwith97@gmail.com>
2025-08-23 14:17:50 -0400

    Completed JavaScript Project 1
    The Completed JavaScript project html file and the js file.

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+// This file demonstrates basic JavaScript functionality for my Basic JavaScript assignment
+
+// The two variables with string values
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+var fullSentence = Sent1 + Sent2;
+
+// Display a window alert with the concatenated sentence
+window.alert(fullSentence);
+
+document.write(fullSentence);
+
+// Expression example (Some simple math)
+document.write("<br>"); 
+document.write("The result of 5 + 7 is: " + (5 + 7));
